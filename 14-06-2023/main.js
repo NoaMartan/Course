59491f64d8957bb425d00c17d47950e014eb1546
noa.martan <noa.martan@is.com>
2023-06-21 14:57:57 +0300

    11 and 14 june changes

---
+++ 14-06-2023/main.js
@@ -0,0 +1,69 @@
+//let num1 = +prompt("please type in a number");
+//let num2 = +prompt("please type in another number");
+//let avg = (num1 + num2) / 2;
+//document.write(`<h1> the average is: ${avg} <h1/>`);
+function makeRequest(location) {
+  return new Promise((resolve, reject) => {
+    console.log(`Making the request to ${location}`);
+    if (location === "Google") {
+      resolve("Google says hi");
+    } else {
+      reject("We can only talk to Google");
+    }
+  });
+}
+
+function processRequest(response) {
+  return new Promise((resolve, reject) => {
+    console.log("Processing request");
+    resolve(`Extra info ${response}`);
+  });
+}
+
+/*
+makeRequest("Facebook")
+  .then((response) => {
+    console.log("Response Received");
+    return processRequest(response);
+  })
+  .then((processedResponse) => {
+    console.log(processedResponse);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+*/
+async function doWork() {
+  try {
+    const response = await makeRequest("Google");
+    console.log("Response Received");
+    const processedResponse = await processRequest(response);
+    console.log(processedResponse);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+//doWork();
+
+function printInterval(number) {
+  return new Promise( (resolve, reject) => {
+    await sleep(20000);
+    for (let i = 1; i <= number; i++) {
+      sleep(2000);
+      resolve(i);
+    }
+    reject("Done");
+  });
+}
+
+async function doWork1() {
+  try {
+    const response = await printInterval(10);
+    console.log(response);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+doWork1(10);
